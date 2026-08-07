a2484e904e48081bd2f9441a47052a5bb91a7fb1
Toon <toonvankimmenade@gmail.com>
2026-08-07 11:20:20 +0200

    Bump version to 0.4.0-xeos (versionCode 20)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.toon.mosaiclink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.3.0-experimental.1"
+        versionCode = 20
+        versionName = "0.4.0-xeos"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
